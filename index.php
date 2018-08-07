5c7746de084307fffdd7f6ffe0113fddecbe3d11
massa <massafranck@gmail.com>
2018-08-07 22:32:34 +0200

    commentaire

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <!--Envoi des données vers la page user.php avec la methode GET (url)-->
     <form  action="user.php" method="get">
       <label for="firstName">Prénom :</label><input type="text" name="firstName" id="firstName"/>
-      <label for="lastName">Nom :</label><input type="text" name="lastName" id="lastName" />
+      <label for="lastName">Nom :</label><input type="text" name="lastName" id="lastName" />black
       <input type="submit" />
     </form>
   </p>
